2ca642a0b001b20276adcc2fe7eaa6ff6ea4e175
Bill Hart <32137104+billhartcivica@users.noreply.github.com>
2018-03-07 15:38:22 +0000

    Create etcdnet.sh

---
+++ etcdnet.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo etcd mkdir /kube-centos/network
+sudo etcdctl mk /kube-centos/network/config "{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }"
